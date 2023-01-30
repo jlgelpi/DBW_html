55c14c080b4f116d7784ec964764c604c42a4075
Josep Ll. Gelpi <gelpi@ub.edu>
2023-01-30 15:18:57 +0100

    Minor style fixes

--- testHTTP.php
+++ testHTTP.php
@@ -6,8 +6,8 @@ Request Method: <?=$_SERVER[REQUEST_METHOD]?><br>
 Query_string: <?=$_SERVER[QUERY_STRING]?><br>
 Post Data:<br>
 <?=$HTTP_RAW_POST_DATA?><br>
-<?foreach (array_keys($_SERVER) as $k) {
-print "$k $_SERVER[$k]<br>";
+<?php foreach (array_keys($_SERVER) as $k) {
+    print "$k $_SERVER[$k]<br>";
 }?>
 </BODY>
 </HTML>
